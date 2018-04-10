51703de0532f38aa2c0e30386bf3cfe5fb7e9dfd
Xavier <Xavier@xaviers-mac-pro.home>
2018-04-10 14:48:29 +0200

    test generation of READMEs.md

--- generateAllREADMEs.sh
+++ generateAllREADMEs.sh
@@ -2,7 +2,7 @@
 dir=$PWD
 find . -type d -mindepth 1 | while read d; do
 	cd $d/ 
-	$dir/generateREADME.sh
+	$dir/generateREADME.sh $dir 
 	cd $dir
 done
 
--- generateREADME.sh
+++ generateREADME.sh
@@ -1,12 +1,16 @@
 #generate README.md for all E.* files in CURRRENT directory (invoked by directory parsing script)
+#getting the rootpath as an argument for proper url generation 
 
-repoUrl=https://raw.githubusercontent.com/xavrr/test/master/
+rel=${PWD//$1/}
+
+repoUr=https://raw.githubusercontent.com/xavrr/test/master/
 directory=${PWD##*/}
+
 
 loaderUrl="http://212.47.238.202/minitel/minitel-loader.html?url="
 rm README.md
 
 mycmd="echo [{}]($loaderUrl"
 
-find . -name 'E.*' -maxdepth 1 | sed 's|^./||' | xargs -I{} echo [{}]\($loaderUrl$repoUrl$directory/{}\) >> README.md 
+find . -name 'E.*' -maxdepth 1 | sed 's|^./||' | xargs -I{} echo [{}]\($loaderUrl$repoUrl$rel/{}\) >> README.md 
 
