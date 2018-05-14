191cd773f3e810bd3001774104644f224bfb0aea
Xavier Rey-Robert <reyrobert@fr.ibm.com>
2018-05-14 18:01:07 +0200

    push all before autogeneration of READMEs and Thumbnails

--- scripts/_generateThumbnails.sh
+++ scripts/_generateThumbnails.sh
@@ -7,11 +7,11 @@ rel=${PWD//$1/}
 repoUrl=https://raw.githubusercontent.com/XReyRobert/VideotexPagesRepository/master/
 directory=${PWD##*/}
 
-if ls E.* 1> /dev/null 2>&1; then
+if ls E.* *.VTX *.VDT *.vdt *.vtx 1> /dev/null 2>&1; then
     rm -fr .thumbnails >> /dev/null
     mkdir .thumbnails
 fi
 
 imgtag="<img src=\"$repoUrl$rel/thumbnails/{}.png\" width=\"300\">"
 
-find . -name 'E.*' -o -name '*.vtx' -o -name '*.VTX' -o -name '*.VDT' -o -name '*.vdt' -maxdepth 1 | sed 's|^./||' | xargs -I{} -P 20 node $1/scripts/_minitel_screenshot.js $repoUrl$rel/{} $PWD/.thumbnails/{}.png
+find . -name 'E.*' -o -name '*.vtx' -o -name '*.VTX' -o -name '*.VDT' -o -name '*.vdt' -maxdepth 1 | sed 's|^./||' | xargs -I{} -P 60 node $1/scripts/_minitel_screenshot.js $repoUrl$rel/{} $PWD/.thumbnails/{}.png
